5d1c8aac25e258356bb364e1d6087cc9d901c456
Federico Gerardi <federico.gerardi03@gmail.com>
2020-07-26 11:56:22 +0200

    Init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import StaffCard from "../components/StaffCard"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <StaffCard profilePic={require("../images/me.jpg")} name="Federico Gerardi" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id consectetur." />
 )
 
 export default IndexPage
