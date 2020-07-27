cbc7c1a1d89c7df3757b33a10cc9b9bce697c1ca
Federico Gerardi <federico.gerardi03@gmail.com>
2020-07-27 03:09:22 +0200

    fix: font bug

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,11 +3,14 @@ import Typography from "typography";
 const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.45,
-  headerFontFamily: ["Raleway Regular 400", "sans-serif"],
-  bodyFontFamily: ["Raleway Regular 400", "sans-serif"]
+  headerFontFamily: ["Raleway", "sans-serif"],
+  bodyFontFamily: ["Raleway", "sans-serif"],
+  googleFonts: [
+    {
+      name: "Raleway",
+      styles: ["400", "700"],
+    },
+  ],
 });
-
-// Insert styles directly into the <head>
-typography.injectStyles();
 
 export default typography;
