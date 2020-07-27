119c96e78c4248ef3f1932ff8bd18dd777590e89
Federico Gerardi <federico.gerardi03@gmail.com>
2020-07-27 14:58:19 +0200

    feat: Component Directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import ProjectCard from "../components/ProjectCard"
+import ProjectCard from "../components/ProjectCard/ProjectCard"
 
 const IndexPage = () => (
   <div>
