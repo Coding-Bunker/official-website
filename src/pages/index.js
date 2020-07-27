be3f7879c0ee768958323f611046e6123df901ec
Federico Gerardi <federico.gerardi03@gmail.com>
2020-07-27 15:01:46 +0200

    feat: Component Directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ContributorCard from "../components/ContributorCard"
+import ContributorCard from "../components/ContributorCard/ContributorCard"
 
 const IndexPage = () => (
   <ContributorCard name="Federico Gerardi" expert="Python Expert" user="Admin" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id consectetur" />
