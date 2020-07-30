2e7a33246cfab069d54a9d740cd48aba62842815
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2020-07-30 17:41:32 +0200

    Cos'è CB
    Componente della sezione Home

---
+++ src/components/CoseCB/CoseCB.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CoseCB.scss"
+
+class CoseCB extends React.Component{
+    render() {
+        return(
+            <div className="container">
+                <h1>Cos'è Coding Bunker</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus alias consectetur deleniti exercitationem explicabo facere illo incidunt labore libero, magni molestias nisi officiis pariatur praesentium quae quisquam temporibus voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium architecto culpa deleniti</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus alias consectetur deleniti exercitationem explicabo facere illo incidunt labore libero, magni molestias nisi officiis pariatur praesentium quae quisquam temporibus voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium architecto culpa deleniti</p>
+            </div>
+        )
+    }
+}
+export default CoseCB
