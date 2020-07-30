d12558b8ac8953b79c58ac83ed3a83408dc7623c
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2020-07-30 17:56:40 +0200

    Cos'è CB - Font
    Aggiustato il font size

--- src/components/CoseCB/CoseCB.js
+++ src/components/CoseCB/CoseCB.js
@@ -5,7 +5,7 @@ class CoseCB extends React.Component{
     render() {
         return(
             <div className="container">
-                <h1>Cos'è Coding Bunker</h1>
+                <span className="Titolo">Cos'è Coding Bunker</span><span>(cosa facciamo)</span>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus alias consectetur deleniti exercitationem explicabo facere illo incidunt labore libero, magni molestias nisi officiis pariatur praesentium quae quisquam temporibus voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium architecto culpa deleniti</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus alias consectetur deleniti exercitationem explicabo facere illo incidunt labore libero, magni molestias nisi officiis pariatur praesentium quae quisquam temporibus voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium architecto culpa deleniti</p>
             </div>
