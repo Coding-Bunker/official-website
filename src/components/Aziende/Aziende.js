af83a5b60b3aeb0325d9e93529d354d51a94f4bb
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2020-07-31 13:11:51 +0200

    Migrazione Bootstrap -> REACT Bootstrap

--- src/components/Aziende/Aziende.js
+++ src/components/Aziende/Aziende.js
@@ -1,26 +1,27 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Aziende.scss"
 
-class Aziende extends React.Component{
-    render() {
-        return(
-            <div className="container-fluid">
-                <div className="row justify-content-around h-100">
-                    <div className="col-md-auto my-auto">
+const Aziende = () =>{
+    return(
+        <div>
+            <Container>
+                <Row className="justify-content-center h-100">
+                    <Col md="auto" my="auto">
                         <img src={require("../../images/Image_3.png")} alt=""/>
-                    </div>
-                    <div className="col-md-auto my-auto">
+                    </Col>
+                    <Col md="auto" my="auto">
                         <img src={require("../../images/Image_4.png")} alt=""/>
-                    </div>
-                    <div className="col-md-auto my-auto">
+                    </Col>
+                    <Col md="auto" my="auto">
                         <img src={require("../../images/Image_5.png")} alt=""/>
-                    </div>
-                    <div className="col-md-auto my-auto">
+                    </Col>
+                    <Col md="auto" my="auto">
                         <img src={require("../../images/Image_6.png")} alt=""/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 export default Aziende
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Aziende from "../components/Aziende/Aziende";
 
