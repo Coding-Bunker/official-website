65c675e19efdd245979f2d939e2826604291a0e7
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2020-07-31 12:55:33 +0200

    Migrazione Bootstrap -> REACT Bootstrap

--- src/components/Progetti/Progetti.js
+++ src/components/Progetti/Progetti.js
@@ -1,32 +1,30 @@
 import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
 import "./Progetti.scss"
 import ProjectCard from "../ProjectCard/ProjectCard";
 
 
-class Progetti extends React.Component{
-    render() {
-        return(
-            <div className="main-container">
-                <span className="titolo-progetti">Alcuni nostri progetti</span>
-                <div className="container-fluid">
-                    <div className="row justify-content-around">
-                        <div className="col-md-auto">
-                            <ProjectCard img={require("../../images/project.png")} title="Progetto 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id" />
-                        </div>
-                        <div className="col-md-auto">
-                            <ProjectCard img={require("../../images/test.png")} title="Progetto 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id" />
-                        </div>
-                        <div className="col-md-auto">
-                            <ProjectCard img={require("../../images/project.png")} title="Progetto 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id" />
-                        </div>
-                    </div>
-                </div>
-                <div className="altri-progetti">
-                    <span className="scopri-progetti">Scopri tutti i progetti</span>
-                </div>
-
+const Progetti = () =>{
+    return(
+        <div className="main-container">
+            <span className="titolo-progetti">Alcuni nostri progetti</span>
+            <Container fluid>
+                <Row className="justify-content-around">
+                    <Col md="auto">
+                        <ProjectCard img={require("../../images/project.png")} title="Progetto 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id" />
+                    </Col>
+                    <Col md="auto">
+                        <ProjectCard img={require("../../images/test.png")} title="Progetto 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id" />
+                    </Col>
+                    <Col md="auto">
+                        <ProjectCard img={require("../../images/project.png")} title="Progetto 1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id" />
+                    </Col>
+                </Row>
+            </Container>
+            <div className="altri-progetti">
+                <span className="scopri-progetti">Scopri tutti i progetti</span>
             </div>
-        )
-    }
+        </div>
+    )
 }
 export default Progetti
