805a5f3e60baa2588327e483ffc6fa16670524bf
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2020-07-31 11:12:16 +0200

    Migrazione Bootstrap -> REACT-Bootstrap

--- src/components/CoseCB/CoseCB.js
+++ src/components/CoseCB/CoseCB.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import "./CoseCB.scss"
 
-class CoseCB extends React.Component{
-    render() {
-        return(
-            <div className="container">
+const CoseCB = () =>{
+    return(
+        <div className="container">
+            <Container>
                 <span className="Titolo">Cos'è Coding Bunker</span><span>(cosa facciamo)</span>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus alias consectetur deleniti exercitationem explicabo facere illo incidunt labore libero, magni molestias nisi officiis pariatur praesentium quae quisquam temporibus voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium architecto culpa deleniti</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus alias consectetur deleniti exercitationem explicabo facere illo incidunt labore libero, magni molestias nisi officiis pariatur praesentium quae quisquam temporibus voluptatum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium architecto culpa deleniti</p>
-            </div>
-        )
-    }
+            </Container>
+        </div>
+    )
 }
-export default CoseCB
+export default CoseCB;
