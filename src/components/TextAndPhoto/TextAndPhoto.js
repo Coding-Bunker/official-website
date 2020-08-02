2d8416c315e9c03c93aacadd8250e5bdcc47aca7
Federico Gerardi <federico.gerardi03@gmail.com>
2020-08-02 11:33:11 +0200

    feat: title and description props

--- src/components/TextAndPhoto/TextAndPhoto.js
+++ src/components/TextAndPhoto/TextAndPhoto.js
@@ -8,8 +8,8 @@ export default props => (
         <Container fluid className={textAndPhotoStyles.containerText}>
             <Row>
                 <Col>
-                    <div className={textAndPhotoStyles.title}>Coding Bunker</div>
-                    <div className={textAndPhotoStyles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin suscipit sodales nulla id consectetur. Mauris quis eleifend ex. Nullam justo nisl, pharetra id fermentum in, pretium sit amet tortor. Integer volutpat nulla sed placerat ultrices. Duis gravida tortor ac eros consectetur, non varius metus egestas.</div>
+                    <div className={textAndPhotoStyles.title}>{props.title}</div>
+                    <div className={textAndPhotoStyles.description}>{props.description}</div>
                 </Col>
                 <div className={textAndPhotoStyles.imgContainer}>
                     <img className={textAndPhotoStyles.img} src={props.img} />
