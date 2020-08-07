8bb35b58a7273849bf021998d2252a7cba7ee927
ArcaneDiver <michele.dellamea@bearzi.org>
2020-08-07 11:22:54 +0200

    refactor: 💡 code refractoring

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,66 +1,64 @@
-import React from "react"
-import eventCardStyles from "./EventCard.module.scss"
-
+import React from 'react'
+import eventCardStyles from './EventCard.module.scss'
 
 export default props => {
+	// Prendo la data e la scompongo in giorno, mese, anno
+	let date = props.date
+	let [day, month, year] = date.split('/')
 
-    // Prendo la data e la scompongo in giorno, mese, anno
-    let date = props.date
-    let [day, month, year] = date.split("/")
+	// Converto il mese da numero a lettere
+	switch (parseInt(month)) {
+		case 1:
+			month = 'GEN'
+			break
+		case 2:
+			month = 'FEB'
+			break
+		case 3:
+			month = 'MAR'
+			break
+		case 4:
+			month = 'APR'
+			break
+		case 5:
+			month = 'MAG'
+			break
+		case 6:
+			month = 'GIU'
+			break
+		case 7:
+			month = 'LUG'
+			break
+		case 8:
+			month = 'AGO'
+			break
+		case 9:
+			month = 'SET'
+			break
+		case 10:
+			month = 'OTT'
+			break
+		case 11:
+			month = 'NOV'
+			break
+		case 12:
+			month = 'DEC'
+			break
+	}
 
-    // Converto il mese da numero a lettere
-    switch(parseInt(month)) {
-        case 1:
-            month = "GEN"
-            break
-        case 2:
-            month = "FEB"
-            break
-        case 3:
-            month = "MAR"
-            break
-        case 4:
-            month = "APR"
-            break
-        case 5:
-            month = "MAG"
-            break
-        case 6:
-            month = "GIU"
-            break
-        case 7:
-            month = "LUG"
-            break
-        case 8:
-            month = "AGO"
-            break
-        case 9:
-            month = "SET"
-            break
-        case 10:
-            month = "OTT"
-            break
-        case 11:
-            month = "NOV"
-            break
-        case 12:
-            month = "DEC"
-            break
-    }
-
-    return (
-        <div className={eventCardStyles.containerCard}>
-            <div className={eventCardStyles.imgContainer}>
-                <img className={eventCardStyles.img} src={props.img} alt={props.title}/>
-            </div>
-            <div className={eventCardStyles.calendarContainer}>
-                <div className={eventCardStyles.month}>{month}</div>
-                <div className={eventCardStyles.day}>{day}</div>
-            </div>
-            <div className={eventCardStyles.descriptionContainer}>
-                <div className={eventCardStyles.title}>{props.title}</div>
-                <div className={eventCardStyles.description}>{props.description}</div>
-            </div>
-        </div>
-    )
+	return (
+		<div className={eventCardStyles.containerCard}>
+			<div className={eventCardStyles.imgContainer}>
+				<img className={eventCardStyles.img} src={props.img} alt={props.title} />
+			</div>
+			<div className={eventCardStyles.calendarContainer}>
+				<div className={eventCardStyles.month}>{month}</div>
+				<div className={eventCardStyles.day}>{day}</div>
+			</div>
+			<div className={eventCardStyles.descriptionContainer}>
+				<div className={eventCardStyles.title}>{props.title}</div>
+				<div className={eventCardStyles.description}>{props.description}</div>
+			</div>
+		</div>
+	)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,28 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { graphql } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from '../components/layout'
+import Image from '../components/image'
 
-const IndexPage = ({ data }) => 
-{
-  return (
-    <Layout>
-      <h1>{data.datoCmsHomepage.sections[0].titolo /*visitare un attributo della sezione numero 0 (ovvero DatoCmsAboutUsSection che non bisogna indicare)*/}</h1>
-      <p>
-        {data.datoCmsHomepage.sections[0].descrizione}
-      </p>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </Layout>
-  )
+const IndexPage = ({ data }) => {
+	return (
+		<Layout>
+			<h1>
+				{
+					data.datoCmsHomepage.sections[0]
+						.titolo /*visitare un attributo della sezione numero 0 (ovvero DatoCmsAboutUsSection che non bisogna indicare)*/
+				}
+			</h1>
+			<p>{data.datoCmsHomepage.sections[0].descrizione}</p>
+			<p>Welcome to your new Gatsby site.</p>
+			<p>Now go build something great.</p>
+			<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+				<Image />
+			</div>
+			<Link to="/page-2/">Go to page 2</Link> <br />
+			<Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+		</Layout>
+	)
 }
 export const query = graphql`
 	{
