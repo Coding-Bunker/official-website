e4667cf3e01b84877e7ee96ecaf9b8310070cedb
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2020-08-11 15:56:26 +0200

    Stili globali fix

--- src/components/Chisiamo/Chisiamo.js
+++ src/components/Chisiamo/Chisiamo.js
@@ -4,10 +4,10 @@ import "./Chisiamo.scss"
 
 const Chisiamo = () =>{
     return(
-        <div>
+        <div className="chi-siamo">
             <img src={require("../../images/banner.png")} alt=""/>
             <Container fluid className="contenitore-testo">
-                <span>Chi siamo</span>
+                <h5>Chi siamo</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis, dignissimos dolores eaque explicabo, hic, illum incidunt iure iusto libero maxime molestias nemo nesciunt perferendis quaerat quidem quisquam sunt voluptatum.Accusantium debitis, dignissimos dolores eaque explicabo, hic, illum incidunt iure iusto libero maxime molestias nemo nesciunt dignissimos dolores eaque explicabo.</p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium debitis, dignissimos dolores eaque explicabo, hic, illum incidunt iure iusto libero maxime molestias nemo nesciunt perferendis quaerat quidem quisquam sunt voluptatum.Accusantium debitis, dignissimos dolores eaque explicabo, hic, illum incidunt iure iusto libero maxime molestias nemo nesciunt dignissimos dolores eaque explicabo.</p>
             </Container>
