b4e3b15bedb2df6370ae49df2217abe584c5a382
ArcaneDiver <michele.dellamea@bearzi.org>
2020-08-11 16:52:11 +0200

    feat: 🎸 Remove initial styles from Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -28,15 +28,7 @@ const Layout = ({ children }) => {
 		<React.Fragment>
 			<Head />
 			<Navbar siteTitle={data.site.siteMetadata.title} />
-			<div
-				style={{
-					margin: `0 auto`,
-					maxWidth: 960,
-					padding: `0 1.0875rem 1.45rem`,
-				}}
-			>
-				<main>{children}</main>
-			</div>
+			<main>{children}</main>
 			<Footer />
 		</React.Fragment>
 	)
