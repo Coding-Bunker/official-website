6d5d873b5694e20b78ab5b07b8448cc3758e0f03
pasqualegiorgio <58633329+pasqualegiorgio@users.noreply.github.com>
2020-09-08 21:12:25 +0200

    Update EntraOraHome.js

--- src/components/EntraOraHome/EntraOraHome.js
+++ src/components/EntraOraHome/EntraOraHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
-import "./EntraOraHome.module.scss"
+import "./EntraOraHome.scss"
 
 const EntraOraHome = () => {
     return(
