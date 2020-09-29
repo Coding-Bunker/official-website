50b1eed01e6f6d4d49ef46f87d644acc14f0a87e
Federico Gerardi <38018471+PyGera@users.noreply.github.com>
2020-09-29 18:52:30 +0200

    fine front-end homepage

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,55 +1,57 @@
-import React from 'react'
-import eventCardStyles from './EventCard.module.scss'
+import React from "react"
+import eventCardStyles from "./EventCard.module.scss"
+
 
 export default props => {
+
 	// Prendo la data e la scompongo in giorno, mese, anno
 	let date = props.date
-	let [day, month, year] = date.split('/')
+	let [day, month, year] = date.split("/")
 
 	// Converto il mese da numero a lettere
-	switch (parseInt(month)) {
+	switch(parseInt(month)) {
 		case 1:
-			month = 'GEN'
+			month = "GEN"
 			break
 		case 2:
-			month = 'FEB'
+			month = "FEB"
 			break
 		case 3:
-			month = 'MAR'
+			month = "MAR"
 			break
 		case 4:
-			month = 'APR'
+			month = "APR"
 			break
 		case 5:
-			month = 'MAG'
+			month = "MAG"
 			break
 		case 6:
-			month = 'GIU'
+			month = "GIU"
 			break
 		case 7:
-			month = 'LUG'
+			month = "LUG"
 			break
 		case 8:
-			month = 'AGO'
+			month = "AGO"
 			break
 		case 9:
-			month = 'SET'
+			month = "SET"
 			break
 		case 10:
-			month = 'OTT'
+			month = "OTT"
 			break
 		case 11:
-			month = 'NOV'
+			month = "NOV"
 			break
 		case 12:
-			month = 'DEC'
+			month = "DEC"
 			break
 	}
 
 	return (
 		<div className={eventCardStyles.containerCard}>
 			<div className={eventCardStyles.imgContainer}>
-				<img className={eventCardStyles.img} src={props.img} alt={props.title} />
+				<img className={eventCardStyles.img} src={props.img} alt={props.title}/>
 			</div>
 			<div className={eventCardStyles.calendarContainer}>
 				<div className={eventCardStyles.month}>{month}</div>
