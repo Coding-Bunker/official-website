9ad294d2fb953ac59fec4bee70aee82dd822c7ba
Raffaele Grieco <62102593+httpraffa24@users.noreply.github.com>
2020-11-02 18:19:40 +0100

    Update del componente aggiornato al nuovo wireframe

--- src/components/Aziende/Aziende.js
+++ src/components/Aziende/Aziende.js
@@ -5,18 +5,26 @@ import './Aziende.scss'
 const Aziende = () => {
 	return (
 		<div className="Aziende">
+			{/* Titolo della sezione "Dicono di noi" */}
+			<div className="intestazione">
+				<div className="linea" />
+				<span className="titolo">I nostri partner</span>
+				<div className="linea" />
+			</div>
+			{/* Sottotitolo della sezione */}
+			<div class="sottotitolo">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
 			<Container>
-				<Row className="align-items-center">
-					<Col>
+				<Row className="align-center">
+					<Col md={6}>
 						<img className="Azienda" src={require('../../images/Image_3.png')} alt="" />
 					</Col>
-					<Col>
+					<Col md={6}>
 						<img className="Azienda" src={require('../../images/Image_4.png')} alt="" />
 					</Col>
-					<Col>
+					<Col md={6}>
 						<img className="Azienda" src={require('../../images/Image_5.png')} alt="" />
 					</Col>
-					<Col>
+					<Col md={6}>
 						<img className="Azienda" src={require('../../images/Image_6.png')} alt="" />
 					</Col>
 				</Row>
