0d130364177f13aa46ecf4b18cdddb4d423dbc6b
Michele Della Mea <michele.dellamea.arcanediver@gmail.com>
2020-11-03 15:28:09 +0100

    Revert "Aggiornamento del compononente al nuovo wireframe"

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { Nav, Navbar as BtNavbar, Button, Container } from 'react-bootstrap'
+import { Nav, Navbar as BtNavbar } from 'react-bootstrap'
+import { faLinkedin, faDiscord, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 import Img from 'gatsby-image'
+
+import LinkButton from '../LinkButton'
 
 import './style.scss'
 
@@ -11,7 +14,7 @@ const Navbar = () => {
 			query NavbarQuery {
 				datoCmsNavbar {
 					logo {
-						fixed(height: 100, width: 100) {
+						fixed(height: 70, width: 70) {
 							...GatsbyDatoCmsFixed
 						}
 					}
@@ -21,42 +24,42 @@ const Navbar = () => {
 	)
 	console.log(data)
 	return (
-		<BtNavbar variant="dark" expand="lg">
-			<Container>
-				<BtNavbar.Brand to="/" as={Link}>
-					<Img
-						style={{ verticalAlign: 'middle' }}
-						imgStyle={{ marginBottom: 0 }}
-						fixed={data.datoCmsNavbar.logo.fixed}
-					/>
-				</BtNavbar.Brand>
-				<BtNavbar.Toggle />
-				<div className="nav-container w-100">
-					<BtNavbar.Collapse>
-						<Nav className="links mr-auto">
-							<Nav.Link to="/chi-siamo" as={Link}>
-								Chi siamo
-							</Nav.Link>
-							<Nav.Link to="/eventi" as={Link}>
-								Gli eventi
-							</Nav.Link>
-							<Nav.Link to="/partner" as={Link}>
-								I partner
-							</Nav.Link>
-							<Nav.Link to="/hall-of-fame" as={Link}>
-								Hall of Fame
-							</Nav.Link>
-							<Nav.Link to="/sostienici" as={Link}>
-								Sostienici
-							</Nav.Link>
-							<Nav.Link to="/blog" as={Link}>
-								Blog
-							</Nav.Link>
-						</Nav>
-						<Button id="entra-nel-bunker">Entra nel bunker</Button>
-					</BtNavbar.Collapse>
-				</div>
-			</Container>
+		<BtNavbar expand="lg" bg="primary" variant="dark">
+			<BtNavbar.Brand to="/" as={Link}>
+				<Img
+					style={{ verticalAlign: 'middle' }}
+					imgStyle={{ marginBottom: 0 }}
+					fixed={data.datoCmsNavbar.logo.fixed}
+				/>
+			</BtNavbar.Brand>
+			<BtNavbar.Toggle />
+			<div className="nav-container w-100">
+				<BtNavbar.Collapse>
+					<Nav className="links mr-auto">
+						<Nav.Link to="/chi-siamo" as={Link}>
+							Chi siamo
+						</Nav.Link>
+						<Nav.Link to="entra-ora" as={Link}>
+							Entra ora
+						</Nav.Link>
+						<Nav.Link to="hall-of-fame" as={Link}>
+							Hall of fame
+						</Nav.Link>
+						<Nav.Link to="/blog" as={Link}>
+							Blog
+						</Nav.Link>
+						<Nav.Link to="/sostienici" as={Link}>
+							Sostienici
+						</Nav.Link>
+					</Nav>
+					<Nav className="link-buttons">
+						<LinkButton link={'/test'} icon={faInstagram} />
+						<LinkButton link={'/test'} icon={faGithub} />
+						<LinkButton link={'/test'} icon={faDiscord} />
+						<LinkButton link={'/test'} icon={faLinkedin} />
+					</Nav>
+				</BtNavbar.Collapse>
+			</div>
 		</BtNavbar>
 	)
 }
