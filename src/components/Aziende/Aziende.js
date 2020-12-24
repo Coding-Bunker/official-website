dd551387b1ed7507612414017b9b12f38d88973e
HttpRaffa24 <raffaele.grieco2403@gmail.com>
2020-12-24 13:33:34 +0100

    "navbar" update and "Aziende" Feature

--- src/components/Aziende/Aziende.js
+++ src/components/Aziende/Aziende.js
@@ -1,36 +1,40 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import Collab1 from '../../images/collab_1.png'
-import Collab2 from '../../images/collab_2.png'
-import Collab3 from '../../images/collab_3.png'
-import Collab4 from '../../images/collab_4.png'
-
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import './Aziende.scss'
 
-const Aziende = (props) => {
-	const partners = []
-	
-	props.partners.forEach(e => {
-		partners.push(<Col md={6}><a href={e.link}><img className="Azienda" src={e.img} alt="" /></a></Col>)
-	});
-
-
+const Aziende = () => {
 	return (
 		<div className="Aziende">
 			{/* Titolo della sezione "Dicono di noi" */}
 			<div className="intestazione">
 				<div className="linea" />
-					<span className="titolo">{props.title}</span>
+				<span className="titolo">I nostri partner</span>
 				<div className="linea" />
 			</div>
 			{/* Sottotitolo della sezione */}
-			<div class="sottotitolo">{props.description}</div>
+			<div class="sottotitolo">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+			<div className="button">
+				<Button className="diventa-partner">Diventa Partner</Button>
+			</div>
+
 			<Container>
 				<Row className="align-center">
-					{partners}
+					<Col md={6}>
+						<img className="Azienda" src={require('../../images/collab_1.png')} alt="" />
+					</Col>
+					<Col md={6}>
+						<img className="Azienda" src={require('../../images/collab_2.png')} alt="" />
+					</Col>
+					<Col md={6}>
+						<img className="Azienda" src={require('../../images/collab_3.png')} alt="" />
+					</Col>
+					<Col md={6}>
+						<img className="Azienda" src={require('../../images/collab_4.png')} alt="" />
+					</Col>
 				</Row>
 			</Container>
 		</div>
 	)
 }
 export default Aziende
+
