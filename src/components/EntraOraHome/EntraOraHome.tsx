501475deb2bab2500d0555f1543fbf56e927a291
Pietro Costanzi Fantini <pietrocostanzi39@gmail.com>
2020-12-26 23:11:39 +0100

    risolto problemi merge

--- src/components/EntraOraHome/EntraOraHome.tsx
+++ src/components/EntraOraHome/EntraOraHome.tsx
@@ -5,35 +5,33 @@ import './EntraOraHome.scss'
 import IconaDiscord from '../../images/icona-discord.png'
 import EntraOra2 from '../../images/Entra_ora_senza_logo_discord.png'
 
-const EntraOraHome = () => {
-	return (
-		<section className="entra-ora-component">
-			<div className="Discord-Icon">
-				<img src={IconaDiscord} alt="discord" />
+const EntraOraHome = (
+	<section className="entra-ora-component">
+		<div className="Discord-Icon">
+			<img src={IconaDiscord} alt="discord" />
+		</div>
+		<Container
+			fluid
+			className="entra-ora"
+			style={{
+				backgroundImage: `url(${EntraOra2})`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+				overflow: 'hidden',
+			}}
+		>
+			<div className="Enter-Text">
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur YEET YEET YEET
+				libero similique commodi? Qui amet doloremque ab libero numquam dicta, explicabo quo commodi
+				voluptatibus, error, earum hic minima architecto nesciunt?
 			</div>
-			<Container
-				fluid
-				className="entra-ora"
-				style={{
-					backgroundImage: `url(${EntraOra2})`,
-					backgroundSize: 'cover',
-					backgroundPosition: 'center',
-					overflow: 'hidden',
-				}}
-			>
-				<div className="Enter-Text">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio consequuntur YEET YEET YEET
-					libero similique commodi? Qui amet doloremque ab libero numquam dicta, explicabo quo
-					commodi voluptatibus, error, earum hic minima architecto nesciunt?
-				</div>
 
-				<div className="center">
-					<Button variant="light" className="entra-ora-btn px-5 py3">
-						Entra Ora
-					</Button>
-				</div>
-			</Container>
-		</section>
-	)
-}
+			<div className="center">
+				<Button variant="light" className="entra-ora-btn px-5 py3">
+					Entra Ora
+				</Button>
+			</div>
+		</Container>
+	</section>
+)
 export default EntraOraHome
