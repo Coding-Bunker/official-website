3e84be37325b7bf5a94bfe8350f9e1827fea859f
Federico Gerardi <federico.gerardi03@gmail.com>
2020-12-26 19:18:55 +0100

    fix: responsive

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -13,30 +13,28 @@ const Argomento: React.FC = ({ children }) => <div className={blog.args}>{childr
 
 const Blog: React.FC<Props> = ({ topics, title, description }) => (
 	<div className={blog.section}>
-		<Container fluid className={blog.container}>
-			<Row>
-				<Col>
-					<div className={blog.title}>{title}</div>
-					<div className={blog.description}>{description}</div>
+		<Row>
+			<Col xs={12} lg md className={blog.textContainer}>
+				<div className={blog.title}>{title}</div>
+				<div className={blog.description}>{description}</div>
+				<div className={blog.argsContainer}>
 					<div className={blog.themes}>Trattiamo questi temi:</div>
-					<div className={blog.argsContainer}>
-						{Array.from({ length: topics.length / 3 })
-							.map((_, i) => topics.slice(i, i + 3))
-							.map((topicGroup, groupIndex) => (
-								<Row key={groupIndex}>
-									{topicGroup.map((topic, topicIndex) => (
-										<Argomento key={topicIndex}>{topic}</Argomento>
-									))}
-								</Row>
-							))}
-					</div>
-				</Col>
-				<Col>
-					<PostCard className={blog.card} />
-					<div className={blog.button}>Vai al Blog</div>
-				</Col>
-			</Row>
-		</Container>
+					{Array.from({ length: topics.length / 3 })
+						.map((_, i) => topics.slice(i, i + 3))
+						.map((topicGroup, groupIndex) => (
+							<Row key={groupIndex}>
+								{topicGroup.map((topic, topicIndex) => (
+									<Argomento key={topicIndex}>{topic}</Argomento>
+								))}
+							</Row>
+						))}
+				</div>
+			</Col>
+			<Col className={blog.right}>
+				<PostCard className={blog.card} />
+				<div className={blog.button}>Vai al Blog</div>
+			</Col>
+		</Row>
 	</div>
 )
 
