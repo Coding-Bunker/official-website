6117d0c9ac04dfdf71558f926c5d5cf8ca9dfef1
Nabster101 <pietrocostanzi39@gmail.com>
2021-01-06 16:01:03 +0100

    Reso responsive 1080p

--- src/components/LoStaff/LoStaff.tsx
+++ src/components/LoStaff/LoStaff.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 
 import StaffCard from '../StaffCard/StaffCard.js'
-import './LoStaff.scss'
+import staffCardStyles from './LoStaff.module.scss'
 
 interface Card {
 	title: string
@@ -19,21 +19,28 @@ interface Props {
 const LoStaff: React.FC<Props> = ({ title, description, cards }) => {
 	return (
 		<div className="Lo-Staff">
-			<div className="Contenitore-Titoli">
-				<div className="Divisore" />
-				<span className="Titolo-Staff">{title}</span>
-				<div className="Divisore" />
+			<div className={staffCardStyles.TitlesContainer}>
+				<div className={staffCardStyles.Seperators} />
+				<span className={staffCardStyles.ComponentTitle}>{title}</span>
+				<div className={staffCardStyles.Seperators} />
 			</div>
 
-			<div className="Contenitore-Descrizione">
-				<span className="Descrizione">{description}</span>
+			<div className={staffCardStyles.DescriptionContainer}>
+				<span className={staffCardStyles.Description}>{description}</span>
 			</div>
 
 			<Container fluid>
-				<Row className="justify-content-around h-100">
+				<Row className="justify-content-around">
 					{cards.map((card, i) => (
 						<Col md="auto" key={i}>
-							<StaffCard profilePic={card.img} name={card.title} description={card.description} />
+							<StaffCard
+								className={staffCardStyles.profielPicContainer}
+								className2={staffCardStyles.name}
+								className3={staffCardStyles.description}
+								profilePic={card.img}
+								name={card.title}
+								description={card.description}
+							/>
 						</Col>
 					))}
 				</Row>
