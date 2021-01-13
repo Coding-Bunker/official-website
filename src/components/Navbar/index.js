90ad8a8f628c6f4ec6ff922f7d6d7781438b19a4
HttpRaffa24 <raffaele.grieco2403@gmail.com>
2021-01-13 18:18:41 +0100

    fix navbar (mobile e tablet)

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Nav, Navbar as BtNavbar, Button, Container } from 'react-bootstrap'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 import Img from 'gatsby-image'
@@ -6,6 +6,10 @@ import Img from 'gatsby-image'
 import './style.scss'
 
 const Navbar = () => {
+	const [isActive, setActive] = useState(false)
+	const toggleBox = () => {
+		setActive(!isActive)
+	}
 	const data = useStaticQuery(
 		graphql`
 			query NavbarQuery {
@@ -20,8 +24,15 @@ const Navbar = () => {
 		`,
 	)
 	console.log(data)
+
 	return (
-		<BtNavbar variant="dark" expand="lg" collapseOnSelect>
+		<BtNavbar
+			variant="dark"
+			expand="lg"
+			className={isActive ? 'sticky-top' : ''}
+			onClick={toggleBox}
+			collapseOnSelect
+		>
 			<Container>
 				<BtNavbar.Brand id="brand" to="/" as={Link}>
 					<Img
@@ -31,7 +42,7 @@ const Navbar = () => {
 					/>
 				</BtNavbar.Brand>
 				<BtNavbar.Toggle />
-				<div className="nav-container w-100" id="div-0">
+				<div className="nav-container w-100" id="mydiv">
 					<BtNavbar.Collapse id="ncollapse">
 						<Nav className="links mr-auto" id="nav-0">
 							<Nav.Link to="/chi-siamo" as={Link}>
