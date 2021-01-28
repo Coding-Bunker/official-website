980138617c5fa537557cdcac17c2d01e320f4781
Federico Gerardi <federico.gerardi03@gmail.com>
2021-01-28 20:22:49 +0100

    feat: entra ora discord section

--- src/pages/entra-ora.js
+++ src/pages/entra-ora.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import EntraOraDiscord from '../components/EntraOraDiscord'
 import Layout from '../components/layout'
 import WhyEnter from '../components/WhyEnter-EntraOraPage/WhyEnter.tsx'
 import WhyEnterImage from '../images/WhyEnterOminide.png' // DA RIMUOVERE POI CON L'INTEGRAZIONE CON IL BACKEND
+import logo from '../images/awesome-discord.png'
 
 const EntraOraPage = () => {
 	return (
 		<Layout>
+			<EntraOraDiscord
+				logo={logo}
+				text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis cum dolorum error hic id inventore, iusto laborum maxime mollitia non officiis provident, quaerat quasi qui, quos sed sequi suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corpo"
+			/>
 			<WhyEnter
 				title="Perché entrare"
 				description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis cum dolorum error hic id inventore, iusto laborum maxime mollitia non officiis provident, quaerat quasi qui, quos sed sequi suscipit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis cum dolorum error hic id inventore."
