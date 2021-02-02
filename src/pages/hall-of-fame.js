dea22205ce6153627c6acc9f5abbf61c24054400
Nabster101 <pietrocostanzi39@gmail.com>
2021-02-02 18:45:30 +0100

    Sezione finita e responsive

--- src/pages/hall-of-fame.js
+++ src/pages/hall-of-fame.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Contributors from '../components/Contributors'
+import Projects from '../components/Progetti-HallofFame/ProgettiHOF.tsx'
+import ProjectImage from '../images/project.png'
 
 const HallOfFame = () => {
+	const projects = []
+	projects.push({
+		description: 'TEST',
+		title: 'TEST',
+		img: ProjectImage,
+	})
 	return (
 		<Layout>
+			<Projects title="I nostri progetti" projects={projects} />
 			<Contributors />
 		</Layout>
 	)
