b0a038b9f8c014276b01c4d342e05d5e758226e2
Nabster101 <pietrocostanzi39@gmail.com>
2021-02-03 19:39:31 +0100

    Dimenticato una Col

--- src/components/Progetti-HallofFame/ProgettiHOF.tsx
+++ src/components/Progetti-HallofFame/ProgettiHOF.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Button, Col, Collapse, Container, Row } from 'react-bootstrap'
+import React from 'react'
+import { Carousel, Container, Row, Col } from 'react-bootstrap'
 import ProgettiHOFStyle from './ProgettiHOF.module.scss'
 import ProjectCard from '../ProjectCard/ProjectCard.js'
 
@@ -15,12 +15,6 @@ interface Props {
 }
 
 const ProgettiHOF: React.FC<Props> = ({ title, projects }) => {
-	const [open, setOpen] = useState(false)
-	const [isActive, setActive] = useState(true)
-	const toggleBox = () => {
-		setActive(!isActive)
-	}
-
 	return (
 		<div className={ProgettiHOFStyle.ProgettiHOF}>
 			<div className={ProgettiHOFStyle.TitlesContainer}>
@@ -30,145 +24,48 @@ const ProgettiHOF: React.FC<Props> = ({ title, projects }) => {
 			</div>
 			<div className={ProgettiHOFStyle.SezioneRosa}>
 				<Container fluid className={ProgettiHOFStyle.Project_Container}>
-					<Row className="justify-content-around">
-						{projects.map((project, i) => (
-							<Col md="auto" className={ProgettiHOFStyle.Project_Column} key={i}>
-								<div
-									className={`${ProgettiHOFStyle} ${
-										isActive
-											? 'ProgettiHOF-module--ScrollButtonContainer1--2HRS4'
-											: 'ProgettiHOF-module--ScrollButtonContainer1Active--QCK9o'
-									}`}
-								>
-									<Button
-										onClick={() => {
-											setOpen(!open)
-											toggleBox()
-										}}
-										className={ProgettiHOFStyle.ScrollButton1}
-									>
-										{'>'}
-									</Button>
-								</div>
-								<div
-									id={ProgettiHOFStyle.Rotate}
-									className={`${ProgettiHOFStyle} ${
-										isActive
-											? 'ProgettiHOF-module--ScrollButtonContainer2Active--3MzJM'
-											: 'ProgettiHOF-module--ScrollButtonContainer2--2HsUe'
-									}`}
-								>
-									<Button
-										onClick={() => {
-											setOpen(!open)
-											toggleBox()
-										}}
-										className={ProgettiHOFStyle.ScrollButton2}
-									>
-										{'>'}
-									</Button>
-								</div>
-
-								<Container fluid>
-									<Row>
-										<Collapse
-											in={!open}
-											className={`${ProgettiHOFStyle} ${
-												isActive
-													? 'ProgettiHOF-module--Collapse1--37URP'
-													: 'ProgettiHOF-module--Collapse1Active--1BZAk'
-											}`}
-										>
-											<Col>
-												<div>
-													<div className={ProgettiHOFStyle.FirstPage}>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-													</div>
-												</div>
-											</Col>
-										</Collapse>
-										<Collapse
-											in={open}
-											className={`${ProgettiHOFStyle} ${
-												isActive
-													? 'ProgettiHOF-module--Collapse2Active--1O1_A'
-													: 'ProgettiHOF-module--Collapse2--zFNUU'
-											}`}
-										>
-											<Col>
-												<div>
-													<div className={ProgettiHOFStyle.SecondPage}>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-														<ProjectCard
-															img={project.img}
-															title={project.title}
-															description={project.description}
-															className={ProgettiHOFStyle.containerCard}
-															className2={ProgettiHOFStyle.description}
-															className3={ProgettiHOFStyle.title}
-														/>
-													</div>
-												</div>
-											</Col>
-										</Collapse>
-									</Row>
-								</Container>
-							</Col>
-						))}
+					<Row>
+						<Col>
+							<Carousel
+								showArrows="true"
+								controls="true"
+								justify-self="end"
+								align-self="center"
+								control-prev-icon-color="invert(100%)"
+								control-next-icon-color="invert(100%)"
+							>
+								<Carousel.Item>
+									<div>
+										{projects.map((project, i) => (
+											<ProjectCard
+												key={i}
+												img={project.img}
+												title={project.title}
+												description={project.description}
+												className={ProgettiHOFStyle.containerCard}
+												className2={ProgettiHOFStyle.description}
+												className3={ProgettiHOFStyle.title}
+											/>
+										))}
+									</div>
+								</Carousel.Item>
+								<Carousel.Item>
+									<div>
+										{projects.map((project, i) => (
+											<ProjectCard
+												key={i}
+												img={project.img}
+												title={project.title}
+												description={project.description}
+												className={ProgettiHOFStyle.containerCard}
+												className2={ProgettiHOFStyle.description}
+												className3={ProgettiHOFStyle.title}
+											/>
+										))}
+									</div>
+								</Carousel.Item>
+							</Carousel>
+						</Col>
 					</Row>
 				</Container>
 			</div>
