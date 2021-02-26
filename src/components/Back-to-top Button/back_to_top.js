c75f3494dc8731d00ec66d78e210ea5ba16933b9
HttpRaffa24 <raffaele.grieco2403@gmail.com>
2021-02-27 00:25:39 +0100

    fix

--- src/components/Back-to-top Button/back_to_top.js
+++ src/components/Back-to-top Button/back_to_top.js
@@ -2,12 +2,23 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
-import './style.scss'
+import './back_to_top.scss'
+
+const handleClick = () => {
+	window.scrollTo({ top: 0, behavior: `smooth` })
+}
 
 const Backtotop = () => {
 	return (
 		<div className="back-to-top-wrapper">
-			<a href="#top" className="back-to-top-link" aria-label="Scroll to Top">
+			<a
+				onClick={handleClick}
+				onKeyDown={handleClick}
+				role="button"
+				tabIndex={0}
+				className="back-to-top-link"
+				aria-label="Scroll to Top"
+			>
 				<FontAwesomeIcon icon={faArrowUp} id="ArrowUp" />
 			</a>
 		</div>
