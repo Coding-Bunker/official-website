4b41dd5219bd6931d43536bac47b9a1be8e3778a
Nabster101 <pietrocostanzi39@gmail.com>
2021-03-10 16:43:54 +0100

    Inizializzata pagina

---
+++ src/components/SostieniciCard/SostieniciCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SostieniciCardStyle from './SostieniciCard.module.scss'
+
+const SostieniciCard = () => {
+	return (
+		<div className={SostieniciCardStyle.SostieniciCardContainer}>
+			<div className={SostieniciCardStyle.SostieniciCardComponent} />
+		</div>
+	)
+}
+export default SostieniciCard
