a595f5e525366c354f7f1b0bb3101a6f375c5abf
Nabster101 <pietrocostanzi39@gmail.com>
2021-03-21 18:31:13 +0100

    Fixato la sezione eventi

--- src/pages/eventi.js
+++ src/pages/eventi.js
@@ -106,7 +106,7 @@ const Eventi = () => {
 					description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
 					cards={newEventi}
 				/>
-				<PastScrollable />
+				<PastScrollable description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
 			</Layout>
 		</div>
 	)
