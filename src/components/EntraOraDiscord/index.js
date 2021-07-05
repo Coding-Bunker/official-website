eb6d1b821a20bb63808adf2a2280c8a0a390c435
Pasquale Giorgio <pasqualegiorgio9868@gmail.com>
2021-07-05 14:37:08 +0200

    Fix many of the problems of Entra Ora's page and add notes in md files inside components folders

--- src/components/EntraOraDiscord/index.js
+++ src/components/EntraOraDiscord/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 import style from './style.module.scss'
 
-const EntraOraDiscord = ({ logo, text }) => (
+const EntraOraDiscord = ({ logo }) => (
 	<div className={style.container}>
 		<Row>
 			<Col className={style.left}>
@@ -17,13 +17,25 @@ const EntraOraDiscord = ({ logo, text }) => (
 					sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
 				/>
 			</Col>
+
 			<Col className={style.right}>
 				<div className={style.imgContainer}>
 					<img className={style.img} src={logo} alt="discord" />
 				</div>
-				<div className={style.text}>{text}</div>
+
+				<div className={style.text}>
+					Entra nel server Discord di Coding Bunker e goditi tutto il meglio di questa community e
+					tutte le opportunità che ha da offrirti!
+					<br />
+					Per farlo, ti basta cliccare al link qui sotto.
+				</div>
+
 				<div className={style.button}>
-					<Button variant="light" className={`${style.btn} entra-ora-btn`}>
+					<Button
+						variant="light"
+						className={`${style.btn} entra-ora-btn`}
+						href="https://discord.com/invite/Ct8WCnQ"
+					>
 						Entra Ora
 					</Button>
 				</div>
